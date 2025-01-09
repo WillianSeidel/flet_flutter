8f8f19155a546705a1d831d5259e1866e3cdbb21
WillianSeidel <willianrobertoseidel@gmail.com>
2025-01-09 10:22:03 -0300

    feat: ranking-user

---
+++ flet_memory/lib/telas/cadastro_user.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChooseNicknameScreen extends StatelessWidget {
+  final String userId;
+
+  ChooseNicknameScreen({required this.userId});
+
+  final TextEditingController nicknameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Escolha seu Nickname')),
+      body: Column(
+        children: [
+          TextField(
+            controller: nicknameController,
+            decoration: InputDecoration(labelText: 'Nickname'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+             // FirebaseFirestore.instance.collection('users').doc(userId).set({
+                'nickname': nicknameController.text,
+                'avatarUrl': 'https://link-do-avatar.com/avatar1.png', // Avatar fixo ou escolha do usuário
+                'score': 0,
+              });
+              Navigator.pop(context); // Retorna à tela inicial
+            },
+            child: Text('Salvar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flet_memory/lib/telas/tela_ranking.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RankingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Ranking')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .orderBy('score', descending: true) // Ordena por pontuação
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final users = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              final nickname = user['nickname'];
+              final avatarUrl = user['avatarUrl'];
+              final score = user['score'];
+
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(avatarUrl),
+                ),
+                title: Text(nickname),
+                subtitle: Text('Score: $score'),
+                trailing: Text('#${index + 1}'), // Posição no ranking
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
